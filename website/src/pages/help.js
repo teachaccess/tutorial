267f8c1058f5a4bf831320892400f81712cc0404
Rachel Ruderman <raquel.rudermano@gmail.com>
2021-10-20 23:55:54 -0700

    Run prettier

--- website/src/pages/help.js
+++ website/src/pages/help.js
@@ -5,12 +5,12 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-const React = require('react');
+const React = require("react");
 
 const CompLibrary = {
-  Container: props => <div {...props}></div>,
-  GridBlock: props => <div {...props}></div>,
-  MarkdownBlock: props => <div {...props}></div>
+  Container: (props) => <div {...props}></div>,
+  GridBlock: (props) => <div {...props}></div>,
+  MarkdownBlock: (props) => <div {...props}></div>,
 };
 
 import Layout from "@theme/Layout";
@@ -19,26 +19,26 @@ const Container = CompLibrary.Container;
 const GridBlock = CompLibrary.GridBlock;
 
 function Help(props) {
-  const {config: siteConfig, language = ''} = props;
-  const {baseUrl, docsUrl} = siteConfig;
-  const docsPart = `${docsUrl ? `${docsUrl}/` : ''}`;
-  const langPart = `${language ? `${language}/` : ''}`;
+  const { config: siteConfig, language = "" } = props;
+  const { baseUrl, docsUrl } = siteConfig;
+  const docsPart = `${docsUrl ? `${docsUrl}/` : ""}`;
+  const langPart = `${language ? `${language}/` : ""}`;
   const docUrl = (doc) => `${baseUrl}${docsPart}${langPart}${doc}`;
 
   const supportLinks = [
     {
       content: `Learn more using the [documentation on this site.](${docUrl(
-        'doc1.html',
+        "doc1.html"
       )})`,
-      title: 'Browse Docs',
+      title: "Browse Docs",
     },
     {
-      content: 'Ask questions about the documentation and project',
-      title: 'Join the community',
+      content: "Ask questions about the documentation and project",
+      title: "Join the community",
     },
     {
       content: "Find out what's new with this project",
-      title: 'Stay up to date',
+      title: "Stay up to date",
     },
   ];
 
@@ -57,4 +57,8 @@ function Help(props) {
   );
 }
 
-export default props => <Layout><Help {...props} /></Layout>;
+export default (props) => (
+  <Layout>
+    <Help {...props} />
+  </Layout>
+);
