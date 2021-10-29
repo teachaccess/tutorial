353a562a6d54e1804491f4cf07df00b41b7c97ce
Alla <alla.konstantinova2@dxc.com>
2021-10-29 23:37:27 +0300

    Added code/menus.mdx

---
+++ website/src/theme/ReactLiveScope/index.js
@@ -0,0 +1,17 @@
+/**
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+import React from 'react';
+import * as MenuHelpers from '../../../../docs/_util/MenuExample';
+// Add react-live imports you need here
+const ReactLiveScope = {
+  React,
+  ...React,
+  MenuHelpers
+};
+
+export default ReactLiveScope;
